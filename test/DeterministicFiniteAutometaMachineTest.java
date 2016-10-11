de4f0d9e695d8d5e7613246fa35c3802beea71ef
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-10-11 13:55:26 +0530

    Added test for invalid alphabet in DFA test

--- test/DeterministicFiniteAutometaMachineTest.java
+++ test/DeterministicFiniteAutometaMachineTest.java
@@ -1,5 +1,7 @@
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.HashSet;
 
@@ -7,41 +9,41 @@ import static org.junit.Assert.*;
 
 public class DeterministicFiniteAutometaMachineTest {
 
-    private TransitionTable transitionTable;
-    private HashSet<State> states;
-    private HashSet<State> finalStates;
-    private HashSet<String> alphabet;
-    private State q0;
-    private State q1;
+    private DeterministicFiniteAutometaMachine deterministicFiniteAutometaMachine;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
 
     @Before
     public void setUp() {
-        q0 = new State("q0");
-        q1 = new State("q1");
+        State q0 = new State("q0");
+        State q1 = new State("q1");
 
-        transitionTable = new TransitionTable();
+        TransitionTable transitionTable = new TransitionTable();
         transitionTable.addTransition(q0, "0", q0);
         transitionTable.addTransition(q1, "0", q0);
         transitionTable.addTransition(q0, "1", q1);
         transitionTable.addTransition(q1, "1", q1);
 
-        states = new HashSet<>();
+        HashSet<State> states = new HashSet<>();
         states.add(q0);
         states.add(q1);
 
-        finalStates = new HashSet<>();
+        HashSet<State> finalStates = new HashSet<>();
         finalStates.add(q1);
 
-        alphabet = new HashSet<>();
+        HashSet<String> alphabet = new HashSet<>();
         alphabet.add("0");
         alphabet.add("1");
+
+        deterministicFiniteAutometaMachine = new DeterministicFiniteAutometaMachine(states, alphabet, transitionTable, q0, finalStates);
 
     }
 
     @Test
     public void shouldAcceptValidString() throws InvalidAlphabetException {
 
-        DeterministicFiniteAutometaMachine deterministicFiniteAutometaMachine = new DeterministicFiniteAutometaMachine(states, alphabet, transitionTable, q0, finalStates);
 
         assertTrue(deterministicFiniteAutometaMachine.validate("01"));
         assertTrue(deterministicFiniteAutometaMachine.validate("1"));
@@ -52,11 +54,16 @@ public class DeterministicFiniteAutometaMachineTest {
     @Test
     public void shouldNotAcceptInvalidString() throws InvalidAlphabetException {
 
-
-        DeterministicFiniteAutometaMachine deterministicFiniteAutometaMachine = new DeterministicFiniteAutometaMachine(states, alphabet, transitionTable, q0, finalStates);
-
         assertFalse(deterministicFiniteAutometaMachine.validate("0"));
         assertFalse(deterministicFiniteAutometaMachine.validate("010101011110"));
+
+    }
+
+    @Test
+    public void shouldThrowInvalidAlphabetExceptionForInvalidAlphabet() throws InvalidAlphabetException {
+        thrown.expect(InvalidAlphabetException.class);
+        thrown.expectMessage("2 is not a valid alphabet");
+        deterministicFiniteAutometaMachine.validate("012");
 
     }
 }
