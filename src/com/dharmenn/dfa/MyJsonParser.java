7b633ec27dc0b8e69a8dc75493cce1757f9b4105
dharmenn <dharmenn@thoughtworks.com>
2016-10-15 12:08:54 +0530

    Removing unnecessary Exception from method signature

--- src/com/dharmenn/dfa/MyJsonParser.java
+++ src/com/dharmenn/dfa/MyJsonParser.java
@@ -1,11 +1,10 @@
 package com.dharmenn.dfa;
+
 import net.minidev.json.JSONArray;
 import net.minidev.json.parser.JSONParser;
 
-import java.io.IOException;
-
 class MyJsonParser {
-    JSONArray parse(String jsonData) throws IOException, net.minidev.json.parser.ParseException {
+    JSONArray parse(String jsonData) throws net.minidev.json.parser.ParseException {
         JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
 
         Object obj = parser.parse(jsonData);
