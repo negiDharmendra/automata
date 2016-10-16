dc0884cd7ed358cf374c64c6d6e691bf651c0263
dharmenn <dharmenn@thoughtworks.com>
2016-10-16 17:59:04 +0530

    Added few more method in MYJsonParser
    extractTuple method to extract all tuple values
    extractDfaInfo to extract dfa info like name and type
    extract testCases to extract all passCases and failCases

--- src/com/dharmenn/dfa/MyJsonParser.java
+++ src/com/dharmenn/dfa/MyJsonParser.java
@@ -1,10 +1,15 @@
 package com.dharmenn.dfa;
 
 import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
-class MyJsonParser {
-    JSONArray parse(String jsonData) throws net.minidev.json.parser.ParseException {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class MyJsonParser {
+    public JSONArray parse(String jsonData) throws net.minidev.json.parser.ParseException {
         JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
 
         Object obj = parser.parse(jsonData);
@@ -15,5 +20,67 @@ class MyJsonParser {
         } else {
             return (JSONArray) obj;
         }
+    }
+
+    public Tuple extractTuple(JSONObject jsonObject) {
+        JSONObject jsonTupleObject = (JSONObject)jsonObject.get("tuple");
+        ArrayList<String> states = (ArrayList<String>) jsonTupleObject.get("states");
+        Tuple tuple = new Tuple();
+
+        tuple.addStartState(new State((String) jsonTupleObject.get("start-state")));
+
+        for (String state : states) {
+            tuple.addState(new State(state));
+        }
+
+
+        ArrayList<String> finalStates = (ArrayList<String>) jsonTupleObject.get("final-states");
+
+        for (String finalState : finalStates) {
+            tuple.addFinalState(new State(finalState));
+        }
+
+
+        ArrayList<String> alphabets = (ArrayList<String>) jsonTupleObject.get("alphabets");
+
+        for (String alphabet : alphabets) {
+            tuple.addAlphabet(alphabet);
+        }
+
+        tuple.addTransitionTable( createTransitionTable(jsonTupleObject));
+
+
+        return tuple;
+    }
+
+    public HashMap<String, String> extractDFAInfo(JSONObject jsonObject) {
+        HashMap<String, String> objectObjectHashMap = new HashMap<>();
+
+        objectObjectHashMap.put("name", (String) jsonObject.get("name"));
+        objectObjectHashMap.put("type", (String) jsonObject.get("type"));
+
+        return objectObjectHashMap;
+    }
+
+    public HashMap<String, ArrayList<String>> extractTestCases(JSONObject jsonObject) {
+        HashMap<String, ArrayList<String>> testCases = new HashMap<>();
+        testCases.put("pass-cases",(ArrayList<String>) jsonObject.get("pass-cases"));
+        testCases.put("fail-cases",(ArrayList<String>) jsonObject.get("fail-cases"));
+        return testCases;
+    }
+
+    private TransitionTable createTransitionTable(JSONObject jsonTupleObject) {
+        TransitionTable transitionTable = new TransitionTable();
+        HashMap<String, HashMap<String, String>> delta = (HashMap) jsonTupleObject.get("delta");
+
+        Set<String> strings = delta.keySet();
+
+        for (String state : strings) {
+            HashMap<String, String> alphabetSatteHashMap = delta.get(state);
+            for (String alphabet : alphabetSatteHashMap.keySet()) {
+                transitionTable.addTransition(new State(state), alphabet, new State(alphabetSatteHashMap.get(alphabet)));
+            }
+        }
+        return transitionTable;
     }
 }
--- test/com/dharmenn/dfa/MyJsonParserTest.java
+++ test/com/dharmenn/dfa/MyJsonParserTest.java
@@ -1,12 +1,35 @@
 package com.dharmenn.dfa;
+
 import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.ParseException;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class MyJsonParserTest {
+
+    private JSONObject jsonObject;
+    private MyJsonParser myJsonParser;
+
+    @Before
+    public void setUp() throws Exception {
+        String jsonString = "{\"name\":\"odd number of zeroes\",\"type\":\"dfa\"," +
+                "\"tuple\":{\"states\":[\"q1\",\"q2\"],\"alphabets\":[\"1\",\"0\"]," +
+                "\"delta\":{\"q1\":{\"0\":\"q2\",\"1\":\"q1\"},\"q2\":{\"0\":\"q1\",\"1\":\"q2\"}}," +
+                "\"pass-cases\":[\"0\",\"000\"]," +
+                "\"fail-cases\":[\"00\",\"0000\"]" +
+                "\"start-state\":\"q1\",\"final-states\":[\"q2\"]}}";
+
+        myJsonParser = new MyJsonParser();
+        jsonObject = (JSONObject) myJsonParser.parse(jsonString).get(0);
+
+    }
+
     @Test
     public void shouldReturnParsedJsonTextAsJsonArray() throws Exception {
         String jsonText = "[{\"name\":\"bar\"}]";
@@ -27,5 +50,40 @@ public class MyJsonParserTest {
         JSONArray parse = new MyJsonParser().parse(jsonText);
         assertEquals(1, parse.size());
         assertEquals("bar", ((HashMap<String, String>) parse.get(0)).get("name"));
+    }
+
+    @Test
+    public void shouldExtractTupleFromGivenJsonObject() throws ParseException {
+
+        Tuple tuple = myJsonParser.extractTuple(jsonObject);
+
+        assertEquals("com.dharmenn.dfa.Tuple", tuple.getClass().getName());
+    }
+
+
+    @Test
+    public void shouldExtractDfaInformation() throws ParseException {
+
+        HashMap<String, String> stringStringHashMap = myJsonParser.extractDFAInfo(jsonObject);
+
+        assertEquals("odd number of zeroes", stringStringHashMap.get("name"));
+        assertEquals("dfa", stringStringHashMap.get("type"));
+    }
+
+    @Test
+    public void shouldExtractDfaTestCases() throws ParseException {
+
+        ArrayList<String> expectedPassCases = new ArrayList<>();
+        expectedPassCases.add("0");
+        expectedPassCases.add("000");
+
+        ArrayList<String> expectedFailCases = new ArrayList<>();
+        expectedFailCases.add("00");
+        expectedFailCases.add("0000");
+
+        HashMap<String, ArrayList<String>> stringStringHashMap = myJsonParser.extractTestCases(jsonObject);
+
+        assertEquals(expectedPassCases, stringStringHashMap.get("pass-cases"));
+        assertEquals(expectedFailCases, stringStringHashMap.get("fail-cases"));
     }
 }
