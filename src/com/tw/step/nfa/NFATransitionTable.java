b2f83b1cb20f57eeebe55ed4f515e9a7e87ab937
dharmenn <dharmenn@thoughtworks.com>
2016-10-25 10:01:31 +0530

    Changing method signature refactored nextSate method in NFATransitionTable

--- src/com/tw/step/nfa/NFATransitionTable.java
+++ src/com/tw/step/nfa/NFATransitionTable.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 public class NFATransitionTable {
     private HashMap<State, HashMap<String, HashSet<State>>> transitionTable = new HashMap<>();
 
-    void addTransition(State inputTransition, String alphabet, HashSet outputTransition) {
+    void addTransition(State inputTransition, String alphabet, HashSet<State> outputTransition) {
         if (transitionTable.containsKey(inputTransition))
             addTransitionForExistingState(inputTransition, alphabet, outputTransition);
         else
@@ -22,11 +22,31 @@ public class NFATransitionTable {
         transitionTable.put(inputTransition, alphabetToTransition);
     }
 
-    private void addTransitionForExistingState(State inputTransition, String alphabet, HashSet outputTransition) {
+    private void addTransitionForExistingState(State inputTransition, String alphabet, HashSet<State> outputTransition) {
         transitionTable.get(inputTransition).put(alphabet, outputTransition);
     }
 
     HashSet<State> nextStates(State initialState, String s) {
-        return transitionTable.get(initialState).get(s);
+        HashSet<State> result = new HashSet<>();
+        HashMap<String, HashSet<State>> stringHashSetHashMap = transitionTable.get(initialState);
+        HashSet<State> e = stringHashSetHashMap.get("e");
+        if (e == null) {
+            return stringHashSetHashMap.get(s);
+        } else {
+            for (State state : e) {
+                HashMap<String, HashSet<State>> hashMap = transitionTable.get(state);
+                if (hashMap != null && hashMap.get(s)!=null)
+                    result.addAll(hashMap.get(s));
+            }
+        }
+        HashSet<State> finalResult = new HashSet<>();
+        finalResult.addAll(result);
+        result.stream().filter(state -> transitionTable.get(state) != null && transitionTable.get(state).get("e") != null).forEach(state -> finalResult.addAll(transitionTable.get(state).get("e")));
+        return finalResult;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() +" : "+ transitionTable;
     }
 }
--- test/com/tw/step/nfa/NFATransitionTableTest.java
+++ test/com/tw/step/nfa/NFATransitionTableTest.java
@@ -11,35 +11,74 @@ public class NFATransitionTableTest {
     @Test
     public void shouldGiveNextSetOfStateForAGivenStateAndAlphabet() {
         State q1 = new State("q1");
-        State q2 = new State("q1");
-        State q3 = new State("q1");
+        State q2 = new State("q2");
+        State q3 = new State("q3");
         NFATransitionTable nfaTransitionTable = new NFATransitionTable();
 
         HashSet<State> states = new HashSet<>();
+
+        states.clear();
         states.add(q1);
         nfaTransitionTable.addTransition(q1,"a",states);
 
-        states.clear();
-        states.add(q2);
-        nfaTransitionTable.addTransition(q1,"b",states);
+        HashSet<State> states1 = new HashSet<>();
+        states1.add(q2);
+        nfaTransitionTable.addTransition(q2,"a",states1);
 
-        states.clear();
-        states.add(q2);
-        nfaTransitionTable.addTransition(q2,"a",states);
+        HashSet<State> states2 = new HashSet<>();
+        states2.add(q3);
+        nfaTransitionTable.addTransition(q2,"b",states2);
 
-        states.clear();
-        states.add(q3);
-        nfaTransitionTable.addTransition(q2,"b",states);
-
-        states.clear();
-        states.add(q1);
-        nfaTransitionTable.addTransition(q3,"a",states);
-
-        nfaTransitionTable.addTransition(q3,"b",new HashSet<State>());
+        HashSet<State> states3 = new HashSet<>();
+        states3.add(q1);
+        nfaTransitionTable.addTransition(q3,"a",states3);
 
 
         HashSet<State> expectedStates = new HashSet<>();
         expectedStates.add(q1);
+
+        assertEquals(expectedStates, nfaTransitionTable.nextStates(q1,"a"));
+    }
+
+
+    @Test
+    public void shouldReturnAllStatesForAGivenStateAndAlphabetForEpsilon() {
+        State q1 = new State("q1");
+        State q2 = new State("q2");
+        State q3 = new State("q3");
+        State q4 = new State("q4");
+        State q5 = new State("q5");
+        State q6 = new State("q6");
+        State q7 = new State("q7");
+        State q8 = new State("q8");
+        State q9 = new State("q9");
+
+        NFATransitionTable nfaTransitionTable = new NFATransitionTable();
+
+        HashSet<State> states = new HashSet<>();
+
+        states.clear();
+        states.add(q1);
+        states.add(q2);
+        states.add(q3);
+        states.add(q4);
+        states.add(q5);
+        states.add(q7);
+        states.add(q9);
+        nfaTransitionTable.addTransition(q1,"e",states);
+
+        HashSet<State> states1 = new HashSet<>();
+        states1.add(q6);
+        nfaTransitionTable.addTransition(q5,"a",states1);
+
+
+        HashSet<State> states2 = new HashSet<>();
+        states2.add(q5);
+        nfaTransitionTable.addTransition(q6,"e",states2);
+
+        HashSet<State> expectedStates = new HashSet<>();
+        expectedStates.add(q6);
+        expectedStates.add(q5);
 
         assertEquals(expectedStates, nfaTransitionTable.nextStates(q1,"a"));
     }
