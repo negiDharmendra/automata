91a6a00e6ffa7d829db0854f1ef00618eaddf42c
dharmenn <dharmenn@thoughtworks.com>
2016-10-25 21:02:28 +0530

    Implemented NFAMachine

---
+++ src/com/tw/step/nfa/NFAMachine.java
@@ -0,0 +1,48 @@
+package com.tw.step.nfa;
+
+import com.tw.step.dfa.State;
+
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class NFAMachine {
+    private final HashSet<State> allStates;
+    private final HashSet<String> alphabets;
+    private final NFATransitionTable nfaTransitionTable;
+    private final State initialState;
+    private final HashSet<State> finalStates;
+
+    public NFAMachine(HashSet<State> allStates, HashSet<String> alphabets, NFATransitionTable nfaTransitionTable, State initialState, HashSet<State> finalStates) {
+
+        this.allStates = allStates;
+        this.alphabets = alphabets;
+        this.nfaTransitionTable = nfaTransitionTable;
+        this.initialState = initialState;
+        this.finalStates = finalStates;
+    }
+
+
+    public boolean validate(String a) {
+        String[] split = a.split("");
+        HashSet<State> states = null;
+        boolean firstAlphabet = true;
+        for (String s : split) {
+            HashSet<State> result = new HashSet<>();
+            if (firstAlphabet) {
+                states = this.nfaTransitionTable.nextStates(initialState, s);
+                firstAlphabet = false;
+            } else {
+                for (State state : states) {
+                    HashSet<State> states1 = this.nfaTransitionTable.nextStates(state, s);
+                    if (states1 != null) result.addAll(this.nfaTransitionTable.nextStates(state, s));
+                }
+                states = result;
+            }
+        }
+
+        assert states != null;
+        Stream<State> states1 = states.stream().filter(finalStates::contains);
+        return states1.findAny().isPresent();
+    }
+}
---
+++ test/com/tw/step/nfa/NFAMachineTest.java
@@ -0,0 +1,89 @@
+package com.tw.step.nfa;
+
+import com.tw.step.dfa.State;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class NFAMachineTest {
+
+    private NFAMachine nfaMachine;
+
+    @Before
+    public void setUp() throws Exception {
+
+        State q1 = new State("q1");
+        State q2 = new State("q2");
+        State q3 = new State("q3");
+
+        NFATransitionTable nfaTransitionTable = new NFATransitionTable();
+
+        HashSet<State> stateSet1 = new HashSet<>();
+        stateSet1.add(q1);
+        nfaTransitionTable.addTransition(q1, "a", stateSet1);
+
+        HashSet<State> stateSet2 = new HashSet<>();
+        stateSet2.add(q2);
+        nfaTransitionTable.addTransition(q1, "b", stateSet2);
+
+
+        HashSet<State> stateSet3 = new HashSet<>();
+        stateSet3.add(q2);
+        nfaTransitionTable.addTransition(q2, "a", stateSet3);
+
+        HashSet<State> stateSet4 = new HashSet<>();
+        stateSet4.add(q3);
+        nfaTransitionTable.addTransition(q2, "b", stateSet4);
+
+        HashSet<State> stateSet5 = new HashSet<>();
+        stateSet5.add(q1);
+        nfaTransitionTable.addTransition(q3, "a", stateSet5);
+
+        HashSet<State> stateSet6 = new HashSet<>();
+        stateSet6.add(q3);
+        nfaTransitionTable.addTransition(q1, "e", stateSet6);
+
+        HashSet<State> stateSet7 = new HashSet<>();
+        stateSet7.add(q1);
+        nfaTransitionTable.addTransition(q1, "", stateSet7);
+
+        HashSet<String> alphabets = new HashSet<>();
+        alphabets.add("a");
+        alphabets.add("b");
+
+        HashSet<State> allStates = new HashSet<>();
+        allStates.add(q1);
+        allStates.add(q2);
+        allStates.add(q3);
+
+        HashSet<State> finalStates = new HashSet<>();
+        finalStates.add(q1);
+
+        nfaMachine = new NFAMachine(allStates, alphabets, nfaTransitionTable, q1, finalStates);
+
+    }
+
+    @Test
+    public void shouldValidateAValidString() {
+        assertTrue(nfaMachine.validate(""));
+        assertTrue(nfaMachine.validate("a"));
+        assertTrue(nfaMachine.validate("aa"));
+        assertTrue(nfaMachine.validate("bba"));
+        assertTrue(nfaMachine.validate("bbaa"));
+        assertTrue(nfaMachine.validate("bbabba"));
+        assertTrue(nfaMachine.validate("baba"));
+    }
+
+    @Test
+    public void shouldValidateAInvalidString() {
+        assertFalse(nfaMachine.validate("b"));
+        assertFalse(nfaMachine.validate("ab"));
+        assertFalse(nfaMachine.validate("bbba"));
+        assertFalse(nfaMachine.validate("baa"));
+    }
+
+}
