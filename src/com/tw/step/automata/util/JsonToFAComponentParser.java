886924cdf07e7dafd216a3646496cdfad617998f
dharmenn <dharmenn@thoughtworks.com>
2016-11-04 09:09:56 +0530

    Implemented JsonToFAComponentParser

--- src/com/tw/step/automata/util/JsonToFAComponentParser.java
+++ src/com/tw/step/automata/util/JsonToFAComponentParser.java
@@ -1,17 +1,93 @@
 package com.tw.step.automata.util;
 
+import com.tw.step.automata.dfa.DFATransitionTable;
+import com.tw.step.automata.nfa.NFATransitionTable;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.*;
 
 public class JsonToFAComponentParser {
 
     private final JSONObject jsonObject;
 
     public JsonToFAComponentParser(String jsonString) {
+
         jsonObject = new JSONObject(jsonString);
     }
 
+    public HashSet<String> parseAlphabets() {
+        HashSet<String> alphabets = new HashSet<>();
+        JSONArray alphabets1 = getTuple().getJSONArray(FAutomata.ALPHABETS.value());
+        alphabets1.forEach(alphabet -> alphabets.add(alphabet.toString()));
+        return alphabets;
+    }
 
-    String parseName() {
-        return jsonObject.getString("name");
+    public TransitionTable parseTransitionFunction(HashMap<String, TransitionTable> transitionTables) {
+        JSONObject delta = getTuple().getJSONObject(FAutomata.DELTA.value());
+        TransitionTable transitionTable = transitionTables.get(jsonObject.getString(FAutomata.TYPE.value()));
+
+        Set<String> strings = delta.keySet();
+        for (String string : strings) {
+            JSONObject jsonObject = delta.getJSONObject(string);
+            if (parseMachineInfo().get(FAutomata.TYPE.value()).equals(FAutomata.NFA.value()))
+                parseNFATransitionFunction(string, jsonObject, transitionTable);
+            else
+                parseDFATransitionFunction(string, jsonObject, transitionTable);
+        }
+        return transitionTable;
+    }
+
+    public HashMap<String, String> parseMachineInfo() {
+        HashMap<String, String> machineInfo = new HashMap<>();
+        machineInfo.put(FAutomata.NAME.value(), jsonObject.getString(FAutomata.NAME.value()));
+        machineInfo.put(FAutomata.TYPE.value(), jsonObject.getString(FAutomata.TYPE.value()));
+        return machineInfo;
+    }
+
+    public HashSet<State> parseFinalStates() {
+        HashSet<State> finalState = new HashSet<>();
+        getTuple().getJSONArray(FAutomata.FINALSTATES.value()).forEach(state -> finalState.add(new State(state.toString())));
+        return finalState;
+    }
+
+    public State parseInitialStates() {
+        return new State(getTuple().getString(FAutomata.STARTSTATE.value()));
+    }
+
+    public HashSet<State> parseAllStates() {
+        HashSet<State> finalState = new HashSet<>();
+        getTuple().getJSONArray(FAutomata.STATES.value()).forEach(state -> finalState.add(new State(state.toString())));
+        return finalState;
+    }
+
+
+    public HashMap<String, List<String>> parseTestCases() {
+        HashMap<String, List<String>> testCases = new HashMap<>();
+        List<String> passCases = new ArrayList<>();
+        List<String> failCases = new ArrayList<>();
+        jsonObject.getJSONArray(FAutomata.PASSCASES.value()).forEach(value -> passCases.add(value.toString()));
+        jsonObject.getJSONArray(FAutomata.FAILCASES.value()).forEach(value -> failCases.add(value.toString()));
+        testCases.put(FAutomata.PASSCASES.value(), passCases);
+        testCases.put(FAutomata.FAILCASES.value(), failCases);
+        return testCases;
+    }
+
+    private void parseDFATransitionFunction(String currentState, JSONObject jsonObject, TransitionTable transitionTable) {
+        for (String key : jsonObject.keySet())
+            ((DFATransitionTable) transitionTable).addTransition(new State(currentState), key, new State(jsonObject.getString(key)));
+    }
+
+    private void parseNFATransitionFunction(String currentState, JSONObject jsonObject, TransitionTable transitionTable) {
+        for (String key : jsonObject.keySet()) {
+            JSONArray jsonArray = jsonObject.getJSONArray(key);
+            HashSet<State> states = new HashSet<>();
+            jsonArray.forEach(state -> states.add(new State(state.toString())));
+            ((NFATransitionTable) transitionTable).addTransition(new State(currentState), key, states);
+        }
+    }
+
+    private JSONObject getTuple() {
+        return jsonObject.getJSONObject(FAutomata.TUPLE.value());
     }
 }
---
+++ test/com/tw/step/automata/util/JsonToFAComponentParserTest.java
@@ -0,0 +1,100 @@
+package com.tw.step.automata.util;
+
+import com.tw.step.automata.dfa.DFATransitionTable;
+import com.tw.step.automata.nfa.NFATransitionTable;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class JsonToFAComponentParserTest {
+
+    private JsonToFAComponentParser jsonToFAComponentParser;
+
+    @Before
+    public void setUp() throws Exception {
+        String jsonString = "{\"name\":\"odd number of zeroes\"," +
+                "\"type\":\"dfa\",\"tuple\":{\"states\":[\"q1\",\"q2\"]," +
+                "\"alphabets\":[\"1\",\"0\"]," +
+                "\"delta\":{\"q1\":{\"0\":\"q2\",\"1\":\"q1\"},\"q2\":{\"0\":\"q1\",\"1\":\"q2\"}}," +
+                "\"start-state\":\"q1\",\"final-states\":[\"q2\"]}," +
+                "\"pass-cases\":[\"0\",\"000\"]," +
+                "\"fail-cases\":[\"00\",\"0000\"]}";
+        jsonToFAComponentParser = new JsonToFAComponentParser(jsonString);
+    }
+
+    @Test
+    public void shouldParseAlphabets() throws Exception {
+        HashSet<String> expected = new HashSet<>();
+        expected.add("0");
+        expected.add("1");
+        assertEquals(expected, jsonToFAComponentParser.parseAlphabets());
+    }
+
+    @Test
+    public void shouldParseMachineInfo() throws Exception {
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put(FAutomata.NAME.value(), "odd number of zeroes");
+        expected.put(FAutomata.TYPE.value(), FAutomata.DFA.value());
+        assertEquals(expected, jsonToFAComponentParser.parseMachineInfo());
+    }
+
+    @Test
+    public void shouldParseTestCases() throws Exception {
+        HashMap<String, List<String>> expected = new HashMap<>();
+        ArrayList<String> passCases = new ArrayList<>();
+        passCases.add("0");
+        passCases.add("000");
+        expected.put(FAutomata.PASSCASES.value(), passCases);
+
+        ArrayList<String> failCases = new ArrayList<>();
+        failCases.add("00");
+        failCases.add("0000");
+        expected.put(FAutomata.FAILCASES.value(), failCases);
+
+        assertEquals(expected, jsonToFAComponentParser.parseTestCases());
+    }
+
+    @Test
+    public void shouldParseInitialState() throws Exception {
+        assertEquals(new State("q1"), jsonToFAComponentParser.parseInitialStates());
+    }
+
+    @Test
+    public void shouldParseFinalState() throws Exception {
+        HashSet<State> expected = new HashSet<>();
+        expected.add(new State("q2"));
+        assertEquals(expected, jsonToFAComponentParser.parseFinalStates());
+    }
+
+    @Test
+    public void shouldParseAllStates() throws Exception {
+        HashSet<State> expected = new HashSet<>();
+        expected.add(new State("q1"));
+        expected.add(new State("q2"));
+        assertEquals(expected, jsonToFAComponentParser.parseAllStates());
+    }
+
+    @Test
+    public void shouldParseTransitionFunction() throws Exception {
+        HashMap<String, TransitionTable> transitionTables = new HashMap<>();
+        transitionTables.put(FAutomata.DFA.value(), new DFATransitionTable());
+
+        DFATransitionTable expected = new DFATransitionTable();
+        State q1 = new State("q1");
+        State q2 = new State("q2");
+        expected.addTransition(q1, "0", q2);
+        expected.addTransition(q1, "1", q1);
+        expected.addTransition(q2, "0", q1);
+        expected.addTransition(q2, "1", q2);
+
+
+        assertEquals(expected, jsonToFAComponentParser.parseTransitionFunction(transitionTables));
+    }
+
+}
